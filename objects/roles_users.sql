55276e1d09a559446a286f56e816e62d24681b38
Luz-88 <164443777+Luz-88@users.noreply.github.com>
2024-05-20 23:29:30 -0300

    Update roles_users.sql

--- objects/roles_users.sql
+++ objects/roles_users.sql
@@ -1,1 +1,29 @@
+USE gestion_citas_dentisalud;
 
+-- Creación de usuarios y contraseñas
+DROP USER IF EXISTS 'admision'@'%';
+DROP USER IF EXISTS 'medico'@'%';
+DROP USER IF EXISTS 'superusuario'@'%';
+
+CREATE USER 'admision'@'%' IDENTIFIED BY 'ingreso1';
+CREATE USER 'medico'@'%' IDENTIFIED BY 'ingreso2';
+CREATE USER 'superusuario'@'%' IDENTIFIED BY 'ingreso3';
+
+-- Creación de roles
+DROP ROLE IF EXISTS gestion_agendas;
+DROP ROLE IF EXISTS visor_agendas;
+DROP ROLE IF EXISTS administrador_bbdd;
+
+CREATE ROLE gestion_agendas;
+CREATE ROLE visor_agendas;
+CREATE ROLE administrador_bbdd;
+
+-- Permisos a cada rol
+GRANT SELECT, INSERT, UPDATE ON gestion_citas_dentisalud.* TO 'admision'@'%';
+GRANT SELECT ON gestion_citas_dentisalud.* TO 'medico'@'%';
+GRANT ALL ON gestion_citas_dentisalud.* TO 'superusuario'@'%';
+
+
+SHOW GRANTS FOR 'admision'@'%';
+SHOW GRANTS FOR 'medico'@'%';
+SHOW GRANTS FOR 'superusuario'@'%';
